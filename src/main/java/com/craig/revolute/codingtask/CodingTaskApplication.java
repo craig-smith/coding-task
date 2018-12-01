beaa9b95983446140fc2e381b418feef8495ea2e
Craig Smith <poland_5@msn.com>
2018-12-01 09:34:56 +0100

    initial project structure

---
+++ src/main/java/com/craig/revolute/codingtask/CodingTaskApplication.java
@@ -0,0 +1,12 @@
+package com.craig.revolute.codingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTaskApplication.class, args);
+    }
+}
