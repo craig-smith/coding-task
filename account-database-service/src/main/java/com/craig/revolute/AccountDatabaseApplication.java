0815992b0f27629a28e5aa10007ed6eadc96c932
Craig Smith <poland_5@msn.com>
2018-12-02 15:11:23 +0100

    renamed service to account-database-service added eureka server and client

--- account-database-service/src/main/java/com/craig/revolute/AccountDatabaseApplication.java
+++ account-database-service/src/main/java/com/craig/revolute/AccountDatabaseApplication.java
@@ -2,12 +2,14 @@ package com.craig.revolute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableJpaRepositories
+@EnableEurekaClient
 public class AccountDatabaseApplication {
 
     public static void main(String[] args) {
